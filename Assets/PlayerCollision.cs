d98510cb711156cbc8d6d3b2381f9f5bc2a97068
quan-illogical <quan.nguyen@terralogic.com>
2020-09-01 15:57:44 +0700

    first commit

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision info)
+    {
+        if (info.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform player;
+    public Transform ground;
+    public float speed = 2000f;
+    public float backwardSpeed = 2000f;
+    public float sidewaysSpeed = 2000f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, speed * Time.deltaTime);
+
+        if (player.position.x >= ground.localScale.x/2-1)
+        {
+            player.position = new Vector3(ground.localScale.x/2-1, player.position.y, player.position.z);
+
+        }
+
+        if (player.position.x <= -ground.localScale.x / 2 + 1)
+        {
+            player.position = new Vector3(-ground.localScale.x / 2 + 1, player.position.y, player.position.z);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
